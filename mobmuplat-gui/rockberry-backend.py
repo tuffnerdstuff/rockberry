4ddf8630fd6cd3ec133fdaf2eb0ad26b26a7f01d
Tuff Nerdstuff <tuffnerdstuff@gmail.com>
2016-04-14 19:44:52 +0000

    Refactored python backend a bit, but it is still to resource hungry

--- mobmuplat-gui/rockberry-backend.py
+++ mobmuplat-gui/rockberry-backend.py
@@ -43,12 +43,12 @@ def remove_root(addr):
 # IN:  /FROMGUI_ROOT/sl/<loop>/<action> [1==down|0==up]
 # OUT: /sl/<loop>/[hit|down|up] <action>
 def fromgui_sl_handler(unused_addr, args):
-    print("G-->ME: {0} {1}".format(unused_addr,args))
+    #print("G-->ME: {0} {1}".format(unused_addr,args))
     loop_action = remove_root(unused_addr).split("/")
     
     # Check if message is well formatted
     if len(loop_action) != 3:
-        print("--> DISCARD: invalid message")
+        #print("--> DISCARD: invalid message")
         return
     
     # Get variables
@@ -59,7 +59,7 @@ def fromgui_sl_handler(unused_addr, args):
     state = ("up" if args == 0 else "down")
     sl_osc_addr = "/sl/{0}/{1}".format(loop,state)
     
-    print("ME-->SL: {0} {1}".format(sl_osc_addr, action))
+    #print("ME-->SL: {0} {1}".format(sl_osc_addr, action))
     msg = osc_message_builder.OscMessageBuilder(address = sl_osc_addr)
     msg.add_arg(action)
     msg = msg.build()
@@ -104,6 +104,7 @@ def fromapp_sl_handler(unused_addr, *args):
         # Propagate Relative Position
         old_pos = get_last_value("loop_pos",loop)
         length = get_last_value("loop_len",loop)
+
         if not old_pos is None:
             rel_pos = 0
             if length != 0:
@@ -141,7 +142,7 @@ def register_sl_updates():
                 # GET
                 msg = osc_message_builder.OscMessageBuilder(address = "/sl/{0}/get".format(loop))
                 msg.add_arg(attr)
-                msg.add_arg("osc.udp://224.0.0.1:54321")
+                msg.add_arg("osc.udp://127.0.0.1:9952")
                 msg.add_arg("/fromAPP/sl")
                 msg = msg.build()
                 client_sl.send(msg)
@@ -152,9 +153,6 @@ def register_sl_updates():
         client_gui.send(msg)
         
         time.sleep(0.1)
-    
-def togui_sl_handler(unused_addr, args):
-    print(unused_addr, args)
 
 def run_as_thread(f):
     t = threading.Thread(target=f)
@@ -169,19 +167,34 @@ if __name__ == "__main__":
     # Dispatcher
     fromgui_dispatcher = dispatcher.Dispatcher()
     fromgui_dispatcher.map(FROMGUI_ROOT+"/sl/*", fromgui_sl_handler)
-    fromgui_dispatcher.map("/fromAPP/sl*", fromapp_sl_handler)
-    fromgui_dispatcher.map("*", catchall_handler)
+    #fromgui_dispatcher.map("*", catchall_handler)
     
-    # Update Loop
-    run_as_thread(register_sl_updates)
+    
     
     # Start Server
     fromgui_server = osc_server.ThreadingOSCUDPServer(
         ("224.0.0.1", 54321), fromgui_dispatcher)
     print("Serving on {}".format(fromgui_server.server_address))
     
-    #t = threading.Thread(target=fromgui_server.serve_forever)
-    #t.daemon = True  # thread dies when main thread (only non-daemon thread) exits.
-    #t.start()
-    fromgui_server.serve_forever()
+    t = threading.Thread(target=fromgui_server.serve_forever)
+    t.daemon = True  # thread dies when main thread (only non-daemon thread) exits.
+    t.start()
+
+
+    #####################
+    # FROM SOOPERLOOPER #
+    #####################
+    # Dispatcher
+    fromsl_dispatcher = dispatcher.Dispatcher()
+    fromsl_dispatcher.map("/fromAPP/sl*", fromapp_sl_handler)
+    
+
+    # Update Loop
+    run_as_thread(register_sl_updates)
+
+    # Start Server
+    fromsl_server = osc_server.ThreadingOSCUDPServer(
+        ("127.0.0.1", 9952), fromsl_dispatcher)
+    print("Serving on {}".format(fromsl_server.server_address))
+    fromsl_server.serve_forever()
     
