c35a7e31de691c5ebe2007e0ee6354b7cce6cb7d
Tuff Nerdstuff <tuffnerdstuff@gmail.com>
2017-04-25 21:56:45 +0200

    Added proper interrupt handling

--- pygame-gui/sl_client.py
+++ pygame-gui/sl_client.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-import sys, threading, time, enum
+import sys, signal, threading, time, enum
 from pythonosc import dispatcher
 from pythonosc import osc_server
 from pythonosc import osc_message_builder
 from pythonosc import udp_client
 
 IP = "127.0.0.1"
-PING_DELAY = 1
+PING_DELAY = 10
 
 class SLClient:
     
@@ -20,6 +20,9 @@ class SLClient:
         self.sl = None
     
     def start(self):
+        
+        self.running = True
+                    
         ##########
         # CLIENT #
         ##########
@@ -38,51 +41,78 @@ class SLClient:
         # Server: start (serve_forever() blocks)
         self.osc_server.serve_forever()
         
-        sys.exit(0)
+    def stop(self):
+        self.running = False
         
+        # unregister loop auto updates
+        if self.sl:
+            for loop in range(self.sl.loop_count):
+                    for attr in Loop.ATTRS:
+                        msg = osc_message_builder.OscMessageBuilder(address = "/sl/{}/unregister_auto_update".format(loop))
+                        msg.add_arg(attr)
+                        msg.add_arg("osc.udp://{}:{}".format(IP,self.osc_server_port))
+                        msg.add_arg("/{}/loop".format(self.osc_server_root))
+                        msg = msg.build()
+                        self.osc_client.send(msg)
+            
         
     def parse_osc_message(self,unused_addr, *args):
-        print("unused_addr:{}".format(unused_addr))
-        print("args:{}".format(args)) 
+        print("IN:{} {}".format(unused_addr,args))
         
         fun = unused_addr.split("/")[-1]
-        if fun == "ping":
+        if fun == "global":
             
-            print ("parsing ping")
-            # Set SL attributes
+            hosturl = args[0]
+            version = args[1]
+            loop_count = args[2]
+            
             if not self.sl:
                 self.sl = SooperLooper()
-            self.sl.hosturl = args[0] 
-            self.sl.version = args[1]
-            self.sl.loop_count = int(args[2])
             
-            # Get loop states
+            # Add/remove loops
+            if loop_count != self.sl.loop_count:
+                self.sl.loops = []
+                for l in range(self.sl.loop_count):
+                    self.sl.loops.append(Loop())
+            
+            # Set SL attributes
+            self.sl.hosturl = hosturl
+            self.sl.version = version
+            self.sl.loop_count = int(loop_count)
+            
+            # Request loop updates
             for loop in range(self.sl.loop_count):
                 for attr in Loop.ATTRS:
+                    
+                    # Get current loop state
+                    msg = osc_message_builder.OscMessageBuilder(address = "/sl/{}/get".format(loop))
+                    msg.add_arg(attr)
+                    msg.add_arg("osc.udp://{}:{}".format(IP,self.osc_server_port))
+                    msg.add_arg("/{}/loop".format(self.osc_server_root))
+                    msg = msg.build()
+                    self.osc_client.send(msg)
+                    
+                    # Register for loop state auto-updates (only triggered when loop actually changes)
                     msg = osc_message_builder.OscMessageBuilder(address = "/sl/{}/register_auto_update".format(loop))
                     msg.add_arg(attr)
                     msg.add_arg(100)
                     msg.add_arg("osc.udp://{}:{}".format(IP,self.osc_server_port))
-                    msg.add_arg("/{}/loop_state".format(self.osc_server_root))
+                    msg.add_arg("/{}/loop".format(self.osc_server_root))
                     msg = msg.build()
                     self.osc_client.send(msg)
-                    print("get attr:{} loop:{}".format(attr,loop))
+                    #print("get attr:{} loop:{}".format(attr,loop))
 
         
     def ping_sl(self):
-        while True:
+        while self.running:
             # Ping
             ping_msg = osc_message_builder.OscMessageBuilder(address = "/ping")
             ping_msg.add_arg("osc.udp://{}:{}".format(IP,self.osc_server_port))
-            ping_msg.add_arg("/{}/ping".format(self.osc_server_root))
+            ping_msg.add_arg("/{}/global".format(self.osc_server_root))
             ping_msg = ping_msg.build()
             self.osc_client.send(ping_msg)
             time.sleep(PING_DELAY)
             
-            
-            
-
-       
 
 class SooperLooper:
     def __init__(self):
@@ -94,7 +124,7 @@ class SooperLooper:
 
 class Loop:
     
-    ATTRS = ("loop_len","loop_pos","state","mute")
+    ATTRS = ("loop_len","loop_pos","state","cycle_len","free_time","total_time","rate_output","in_peak_meter","out_peak_meter","is_soloed")
     
     class Mode(enum.Enum):
         UNKNOWN = -1
@@ -120,6 +150,16 @@ class Loop:
         self.mode = self.Mode.UNKNOWN
         
 
+def handle_exit(signal=None,frame=None):
+    print("Quitting ...")
+    c.stop()
+    sys.exit(0)
+
 if __name__ == '__main__':
-    c = SLClient(9951, 9952, "mygui")
-    c.start()
+    try:
+        signal.signal(signal.SIGINT, handle_exit)
+        
+        c = SLClient(9951, 9952, "mygui")
+        c.start()
+    except KeyboardInterrupt:
+            handle_exit()
